c9612baf112dd7b86c59c6124f6c4770fbce6d7a
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-10-01 08:55:40 -0300

    Criação de DataContext e mapeamento da classe Department.

---
+++ Locatudo.Infra/Data/LocatudoDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Locatudo.Domain.Entities;
+using Locatudo.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locatudo.Infra.Data
+{
+    public class LocatudoDataContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public LocatudoDataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new DepartmentMap());
+        }
+    }
+}
---
+++ Locatudo.Infra/Data/Mappings/DepartmentMap.cs
@@ -0,0 +1,34 @@
+﻿using Locatudo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Locatudo.Infra.Data.Mappings
+{
+    public class DepartmentMap : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.ToTable("departments");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("id")
+                .HasColumnType("uuid")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasColumnType("VARCHAR")
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.OwnsOne(x => x.Email)
+                .Property(x => x.Address)
+                .HasColumnName("email")
+                .HasColumnType("VARCHAR")
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
