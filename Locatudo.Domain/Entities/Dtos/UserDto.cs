6d7a8011490d471c9b1d67175308cb2bc166871b
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-10-18 23:13:16 -0300

    Criação de UserDto. Criação de mapeamento de User para UserDto.

---
+++ Locatudo.Domain/Entities/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Locatudo.Domain.Entities.Dtos
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Locatudo.Domain/Entities/Profiles/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Locatudo.Domain.Entities.Dtos;
+
+namespace Locatudo.Domain.Entities.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(
+                    r => r.FirstName,
+                    opt => opt.MapFrom(x => x.Name.FirstName))
+                .ForMember(
+                    r => r.LastName,
+                    opt => opt.MapFrom(x => x.Name.LastName))
+                .ForMember(
+                    r => r.Email,
+                    opt => opt.MapFrom(x => x.Email.Address));
+        }
+    }
+}
