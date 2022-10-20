0389f93f18f5e3d15d67643765928a4a690e5b36
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-10-19 23:47:31 -0300

    Documentação Swagger de retornos de EquipmentController.

--- Locatudo.Api/Controllers/EquipmentController.cs
+++ Locatudo.Api/Controllers/EquipmentController.cs
@@ -3,14 +3,17 @@ using Locatudo.Domain.Handlers;
 using Microsoft.AspNetCore.Mvc;
 using Locatudo.Domain.Repositories;
 using Locatudo.Domain.Entities.Dtos;
+using Locatudo.Domain.Handlers.Commands.Outputs;
 
 namespace Locatudo.Api.Controllers
 {
     [ApiController]
     [Route("v1/equipments")]
+    [Produces("application/json")]
     public class EquipmentController : Controller
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<EquipmentDto>))]
         public IActionResult List([FromServices] IEquipmentRepository equipmentRepository)
         {
             var equipments = equipmentRepository.List<EquipmentDto>();
@@ -18,22 +21,30 @@ namespace Locatudo.Api.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(IEnumerable<CreateEquipmentCommandResponse>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(IReadOnlyCollection<string>))]
         public IActionResult Create(
             [FromServices] CreateEquipmentHandler handler,
             [FromBody] CreateEquipmentCommand command)
         {
             var response = handler.Handle(command);
-            return new OkObjectResult(response);
+            if (!response.Success)
+                return new BadRequestObjectResult(response.Messages);
+            return new CreatedResult("", response.Data);
         }
 
         [HttpPut]
         [Route("change_manager")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ChangeEquipmentManagerCommandResponse>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(IReadOnlyCollection<string>))]
         public IActionResult ChangeManager(
             [FromServices] ChangeEquipmentManagerHandler handler,
             [FromBody] ChangeEquipmentManagerCommand command)
         {
             var response = handler.Handle(command);
-            return new OkObjectResult(response);
+            if (!response.Success)
+                return new BadRequestObjectResult(response.Messages);
+            return new OkObjectResult(response.Data);
         }
     }
 }
