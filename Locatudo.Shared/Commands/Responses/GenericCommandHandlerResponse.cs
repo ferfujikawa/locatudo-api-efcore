7b60e7914fda03ebaa373eb2dc8e6c9451d1a091
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-11-03 21:46:42 -0300

    Refatoração de GenericCommandHandler.

--- Locatudo.Shared/Commands/Responses/GenericCommandHandlerResponse.cs
+++ Locatudo.Shared/Commands/Responses/GenericCommandHandlerResponse.cs
@@ -8,21 +8,21 @@ namespace Locatudo.Shared.Commands.Responses
         public T? Data { get; private set; }
         public IReadOnlyCollection<string> Messages => _messages;
 
-        private readonly IReadOnlyCollection<string> _messages = new List<string>();
+        private readonly IReadOnlyCollection<string> _messages;
 
-        public GenericCommandHandlerResponse(IEnumerable<ValidationFailure> validationFailures)
+        public GenericCommandHandlerResponse(IReadOnlyCollection<ValidationFailure> validationFailures)
         {
             Success = false;
-            _messages = validationFailures.Select(x => x.ErrorMessage).ToList() ;
+            _messages = validationFailures.Select(x => x.ErrorMessage).ToList();
         }
 
-        public GenericCommandHandlerResponse(string? errorMessage)
+        public GenericCommandHandlerResponse(string errorMessage)
         {
             Success = false;
-            _messages = new List<string>() { errorMessage };
+            _messages = new List<string>() { errorMessage  };
         }
 
-        public GenericCommandHandlerResponse(T? data, string? message)
+        public GenericCommandHandlerResponse(T? data, string message)
         {
             Success = true;
             Data = data;
